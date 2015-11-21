e490b7e5d8c13be4423a8af1614505a94a23a580
Travis Pew <travisp@gmail.com>
2015-11-21 13:26:48 -0500

    Abstract BitcraftPanel

--- src/main/java/net/littlebits/ui/PlayInfoPanel.java
+++ src/main/java/net/littlebits/ui/PlayInfoPanel.java
@@ -17,62 +17,22 @@ import java.io.File;
 import java.io.IOException;
 
 
-public class PlayInfoPanel extends JPanel {
+public class PlayInfoPanel extends BitcraftPanel {
 
-    final private ResourceLoader resources;
-    final private LauncherDirectories directories;
+
     private ActionListener loadListener = null;
     private ActionButton downloadButton;
 
-    public static final Color COLOR_LITTLEBITS_ORANGE = new Color(255,138,0); // #FF8A00
-    public static final Color COLOR_LITTLEBITS_BUTTON_HOVER = new Color(230,140,0); // #e67c00
-    public static final Color COLOR_LITTLEBITS_WHITE = new Color(255,255,255);
-    public static final Color COLOR_LITTLEBITS_TEXT = new Color(68,68,68); // #444
-
     public PlayInfoPanel(final ResourceLoader loader,final LauncherDirectories directories) {
-        setLayout(new BorderLayout());
-        this.resources = loader;
-        this.directories = directories;
-
-        this.setFont(resources.getFont(ResourceLoader.FONT_OPENSANS, 16));
-
-        JPanel background = new JPanel(null);
-        background.setOpaque(true);
-        background.setForeground(COLOR_LITTLEBITS_WHITE);
-        background.setBackground(LauncherFrame.COLOR_GREEN);
-        background.setLayout(new BorderLayout());
-        //background.setFilterImage(true);
-        this.add(background, BorderLayout.CENTER);
-
-        BufferedImage backgroundImage = resources.getImage("BITCRAFT-purple-HR.png");
-
-        ImagePanel containerPanel = new ImagePanel(new GridBagLayout());
-        containerPanel.setBackground(Color.green);
-        containerPanel.setImage(backgroundImage);
-        containerPanel.setOpaque(true);
-
-
-        background.add(containerPanel);
-
-
-        JPanel contentPanel = new JPanel();
-        contentPanel.setBackground(COLOR_LITTLEBITS_WHITE);
-        contentPanel.setForeground(COLOR_LITTLEBITS_TEXT);
-        Dimension contentDimensions = new Dimension(500, 320);
-        contentPanel.setPreferredSize(contentDimensions);
-        contentPanel.setMaximumSize(contentDimensions);
-        contentPanel.setMinimumSize(contentDimensions);
-
-        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        super(loader, directories, new Dimension(500, 320));
 
         JLabel goLabel = new JLabel("LET'S GO");
         goLabel.setAlignmentX(CENTER_ALIGNMENT);
-        contentPanel.add(goLabel);
+        this.contentPanel.add(goLabel);
 
         downloadButton = setupDownloadButton();
-        contentPanel.add(downloadButton);
+        this.contentPanel.add(downloadButton);
 
-        containerPanel.add(contentPanel);
     }
 
     public void addDownloadActionListener(ActionListener buttonActionListener) {
